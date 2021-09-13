084a0cf4494aebc407aaa4841bcf310956f1da8a
Akvamarinika <rozespink@mail.ru>
2021-09-13 15:35:47 +0800

    UPD: remove comment

--- merge_sort/src/main/java/com/akvamarin/merge_sorting/merge/MergeFiles.java
+++ merge_sort/src/main/java/com/akvamarin/merge_sorting/merge/MergeFiles.java
@@ -1,77 +1,36 @@
 package com.akvamarin.merge_sorting.merge;
 import com.akvamarin.merge_sorting.exceptions.IncorrectlySortedFileException;
-import org.jetbrains.annotations.Nullable;
-
 import java.io.*;
 import java.util.Optional;
 
 public class MergeFiles<T extends Comparable<T>> {
-    ReadersQueue<T> readersQueue;
+    private final ReadersQueue<T> readersQueue;
 
     public MergeFiles(ReadersQueue<T> readersQueue){
         this.readersQueue = readersQueue;
     }
 
-
     public void mergeSortedFiles(File output){
-
         try (BufferedWriter outputBufferedWriter = new BufferedWriter(new FileWriter(output))) {
-            while (!readersQueue.isEmpty()) {
 
+            while (!readersQueue.isEmpty()) {
                 try {
+
                    Optional<T> optionalElement = readersQueue.getMinMaxElement();
                    if (optionalElement.isPresent()){
                        T minMaxElement = optionalElement.get();
-                       //System.out.println(minMaxElement);
-                       outputBufferedWriter.write(minMaxElement.toString() + System.lineSeparator()); //int
+                       outputBufferedWriter.write(minMaxElement.toString() + System.lineSeparator());
                        readersQueue.setPreviousLineInFile(minMaxElement);
                    }
+
                 } catch (IncorrectlySortedFileException e) {
-                    System.out.println(e.getMessage() + e.getLine());
-                    //continue;
+                    System.err.println(e.getMessage() + e.getLine());
                 }
-                // System.out.println(minLine);
-
-
-              // String nextLine = readersQueue.next();
-
-              /*  if (nextLine != null && minLine > Integer.parseInt(nextLine)) {
-                    linesReadersQueue.offer(new AbstractMap.SimpleImmutableEntry<>(Integer.parseInt(nextLine), bufferedReader));
-                } else {
-                    bufferedReader.close();
-                }*/
             }
-           // outputBufferedWriter.close();
+
         } catch(IOException ex){
-            ex.printStackTrace();
+            System.err.println("Не удалась запись в файл " + output.getName());
         }
+
     }
-
-
-
-
-  /* private <T>  Queue<Map.Entry<T,BufferedReader>> getInitQueue() throws IOException {
-        Comparator sortComparator = options.getSortComparator();
-        List<File> inputFiles = options.getInputFiles();
-        Queue<Map.Entry<T,BufferedReader>> linesReadersQueue = new PriorityQueue<>(inputFiles.size(), sortComparator);
-
-        for (File file : inputFiles){
-            BufferedReader buffReader = new BufferedReader(new FileReader(file));
-                String firstLine = buffReader.readLine();
-
-                if (firstLine != null) {
-                    linesReadersQueue.offer(new AbstractMap.SimpleImmutableEntry<>(Integer.parseInt(firstLine), buffReader));  //int
-                } else {
-                    buffReader.close();
-                }
-        }
-        return linesReadersQueue;
-    }
-
-    private int addQueue(String string){
-        if (options.getDataType() == DataType.INTEGER && DataValidator.isInteger(string)){
-            return Integer.parseInt(string);
-        }
-
-    }*/
 }
