7db91230cc10ca10f07e6c4b7f0c6cd2377dffea
Akvamarinika <rozespink@mail.ru>
2021-09-13 15:27:57 +0800

    UPD: Added handling empty strings and strings that do not convert to numbers. Refactoring method getInitQueue, remove methods: (add, getElement, getBufferReader).

--- merge_sort/src/main/java/com/akvamarin/merge_sorting/merge/ReadersQueue.java
+++ merge_sort/src/main/java/com/akvamarin/merge_sorting/merge/ReadersQueue.java
@@ -1,107 +1,95 @@
 package com.akvamarin.merge_sorting.merge;
+import com.akvamarin.merge_sorting.exceptions.EmptyLineException;
 import com.akvamarin.merge_sorting.exceptions.IncorrectlySortedFileException;
 import com.akvamarin.merge_sorting.options.Options;
 import com.akvamarin.merge_sorting.options.comparators.SortASC;
 import com.akvamarin.merge_sorting.options.comparators.SortDESC;
 import com.akvamarin.merge_sorting.options.params_app.TypeSort;
 import com.akvamarin.merge_sorting.validators.DataValidator;
-import com.akvamarin.merge_sorting.validators.TypeValidator;
+import org.jetbrains.annotations.Nullable;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
-public abstract class ReadersQueue<K extends Comparable<K>> implements TypeValidator {
+public abstract class ReadersQueue<K extends Comparable<K>> {
     private final Queue<Map.Entry<K, BufferedReader>> linesReadersQueue;
     private final Options options;
+    private K previousLineInFile = null;
 
-    public ReadersQueue(Options options) throws IOException {
+    public ReadersQueue(Options options){
         this.options = options;
 
         Comparator<Map.Entry<K, BufferedReader>> sortComparator =
                 (this.options.getTypeSort() == TypeSort.ASC) ? new SortASC<>() : new SortDESC<>();
 
         this.linesReadersQueue = new PriorityQueue<>(this.options.getInputFiles().size(), sortComparator);
-        this.getInitQueue();
+
+        try {
+            this.getInitQueue(options.getInputFiles());
+        } catch (IOException e) {
+            System.err.println(String.format("Ошибка при чтении файла: %s", e.getMessage()));
+        }
     }
 
-    private void getInitQueue() throws IOException {
+    private void getInitQueue(List<File> inputFiles) throws IOException {
         linesReadersQueue.clear();
-        List<File> inputFiles = options.getInputFiles();
-        List<File> filterInputFiles = DataValidator.filterNotFoundFiles(inputFiles);
 
-        for (File file : filterInputFiles){
+        for (File file : inputFiles){
             BufferedReader buffReader = new BufferedReader(new FileReader(file));
-            K firstLine = convert(buffReader.readLine());
-            this.add(buffReader, firstLine);
-
-          /*  if (firstLine != null) {
-
-                linesReadersQueue.offer(new AbstractMap.SimpleImmutableEntry<K, BufferedReader>(Integer.parseInt(firstLine), buffReader));  //int
-            } else {
-                buffReader.close();
+            addNextElementInQueue(buffReader);
+           /* String firstLine = buffReader.readLine();
+            if (firstLine != null){
+                linesReadersQueue.offer(new AbstractMap.SimpleImmutableEntry<>(convert(firstLine), buffReader));
             }*/
         }
     }
 
-    private void add(BufferedReader buffReader, K line) throws IOException {
+    private void addNextElementInQueue(BufferedReader buffReader) throws IOException{
+        String nextLine =  buffReader.readLine();
+
+        try {
+            if (nextLine != null) {
+                K nextLineConvert = convert(nextLine);
+                linesReadersQueue.offer(new AbstractMap.SimpleImmutableEntry<>(nextLineConvert, buffReader));
+            }
+        } catch (NumberFormatException | EmptyLineException e) {
+            System.err.println(e.getMessage());
+            addNextElementInQueue(buffReader);
+        }
+    }
+
+    @Nullable
+    public Optional<K> getMinMaxElement() throws IOException, IncorrectlySortedFileException {
+        Map.Entry<K, BufferedReader> elemReader = linesReadersQueue.poll();
+        BufferedReader buffReader = elemReader.getValue();
+        K line = elemReader.getKey();
+       // System.out.println(previousLineInFile);
+       // System.out.println(line);
+        addNextElementInQueue(buffReader);
+
         if (line != null) {
+            if (DataValidator.isCorrectOrderData(options.getTypeSort(),  previousLineInFile, line)){
+                return Optional.of(line);
+            } else {
+                throw new IncorrectlySortedFileException("Нарушен порядок сортировки, данная строка будет пропущена: ", line.toString());
+            }
 
-
-
-
-           // if (options.getDataType() == DataType.INTEGER && DataValidator.isInteger(line)) {
-                //int number = Integer.parseInt(line);
-
-                linesReadersQueue.offer(new AbstractMap.SimpleImmutableEntry<>(line, buffReader));
-            //}
         } else {
             buffReader.close();
         }
-
-    }
-
-   /* public String next() throws IOException {
-        BufferedReader buffReader = getBufferReader();
-        String nextLine = buffReader.readLine();
-        this.add(buffReader, nextLine);
-        return buffReader.readLine();
-    }*/
-
-    public K getMinMaxElement() throws IOException, IncorrectlySortedFileException {
-        Map.Entry<K, BufferedReader> elemReader = linesReadersQueue.poll();
-        assert elemReader != null;
-        BufferedReader buffReader = elemReader.getValue();
-        K line = elemReader.getKey();
-        K nextLine = convert(buffReader.readLine());
-        this.add(buffReader, nextLine);
-
-        if (TypeValidator.isCorrectOrderData(options.getTypeSort(), line, nextLine)){
-            return nextLine;
-        } else {
-            throw new IncorrectlySortedFileException("Нарушен порядок сортировки, данная строка будет пропущена: ", nextLine.toString());
-        }
-
-    }
-
-
-    public K getElement() {
-        assert linesReadersQueue.peek() != null;
-        return linesReadersQueue.peek().getKey();
-    }
-
-    private BufferedReader getBufferReader() {
-        assert linesReadersQueue.peek() != null;
-        return linesReadersQueue.peek().getValue();
+        return Optional.empty();
     }
 
     public boolean isEmpty() {
         return linesReadersQueue.isEmpty();
     }
 
-    protected abstract K convert(String string) throws NumberFormatException;
+    protected abstract K convert(String string) throws NumberFormatException, EmptyLineException;
 
-
+    public void setPreviousLineInFile(K previousLineInFile) {
+        this.previousLineInFile = previousLineInFile;
+    }
 }
 
